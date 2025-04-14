ab8ad54338a0ad08050c6d99cf08245eb9bc596f
Leonardo Higuti <lyhiguti@gmail.com>
2025-04-14 16:54:55 -0300

    config: finish live reload server

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,7 +1,10 @@
 import path from 'path';
 import { app, BrowserWindow } from 'electron';
+import { isDev } from './util.js';
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({});
-  mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
+  if (isDev()) mainWindow.loadURL('http://localhost:5123');
+  else
+    mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
 });
